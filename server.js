b112de00761b8dfb4ff9d0b273c8f7a3b113dbee
virgoaugustine <virgoaugustine@gmail.com>
2020-06-02 14:49:10 +0000

    fix

--- server.js
+++ server.js
@@ -17,9 +17,7 @@ const db = require('knex')({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-          }
+        ssl: true
        
     }
 });
